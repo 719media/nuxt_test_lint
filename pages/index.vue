24adbc1bb0fd8cef4fcfb04743db6d465c273402
719media <ben@719media.com>
2024-05-08 20:50:07 +0000

    test pages directory

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+const someVariable = 'apple';
+const someOtherVariableWithoutSemicolon = 'banana'
+  const someOtherVariableIndentedWrong = 'cucumber';
+</script>
+<template>
+  <div>
+    <div v-text="someVariable" />
+    <div v-text="someOtherVariableWithoutSemicolon" />
+    <div v-text="someOtherVariableIndentedWrong" />
+  </div>
+</template>
