d8538add4cadff8c84d69ee95250de08674ecf45
719media <ben@719media.com>
2024-05-08 20:49:57 +0000

    add eslint config with stylistic rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"]
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        quotes: 'single',
+        semi: true,
+      },
+    },
+  },
 })
